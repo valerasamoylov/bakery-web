80743fc57eb52ac1cdc90c2254d3e57fa94b352f
Valeriy Samoilov <valerasam1012@gmail.com>
2020-03-20 13:17:57 +0200

    add sticky header after scrolling and section.menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,8 @@
-let mainNav = document.getElementById("js-menu");
-let navBarToggle = document.getElementById("js-navbar-toggle");
-
-if (navBarToggle) {
-  navBarToggle.addEventListener("click", function() {
-    mainNav.classList.toggle("active");
-  });
-}
-
 $(".owl-carousel").owlCarousel({
   loop: true,
-  nav: true,
+  autoplay: true,
+  animateOut: "fadeOut",
+  autoHeight: true,
   responsive: {
     0: {
       items: 1
@@ -20,5 +13,75 @@ $(".owl-carousel").owlCarousel({
     1000: {
       items: 1
     }
+  }
+});
+
+var a = 0;
+$(window).scroll(function() {
+  var oTop = $(".section-counter").offset().top - window.innerHeight;
+  if (a == 0 && $(window).scrollTop() > oTop) {
+    $(".number").each(function() {
+      var $this = $(this),
+        countTo = $this.attr("data-count");
+      $({
+        countNum: $this.text()
+      }).animate(
+        {
+          countNum: countTo
+        },
+
+        {
+          duration: 2000,
+          easing: "swing",
+          step: function() {
+            $this.text(Math.floor(this.countNum));
+          },
+          complete: function() {
+            $this.text(this.countNum);
+            //alert('finished');
+          }
+        }
+      );
+    });
+    a = 1;
+  }
+});
+
+$("#video").videoPopup();
+
+mapboxgl.accessToken =
+  "pk.eyJ1IjoidmFsZXJhc2FtIiwiYSI6ImNrN3l1ZnA5dDAxcjczZG53MXgzdThwM3oifQ.R7o7mzS1jB-qYrKdjX8jaA";
+var mapboxClient = mapboxSdk({ accessToken: mapboxgl.accessToken });
+mapboxClient.geocoding
+  .forwardGeocode({
+    query: "Lviv, Ukraine",
+    autocomplete: false,
+    limit: 1
+  })
+  .send()
+  .then(function(response) {
+    if (
+      response &&
+      response.body &&
+      response.body.features &&
+      response.body.features.length
+    ) {
+      var feature = response.body.features[0];
+
+      var map = new mapboxgl.Map({
+        container: "map",
+        style: "mapbox://styles/mapbox/streets-v11",
+        center: feature.center,
+        zoom: 10
+      });
+      new mapboxgl.Marker().setLngLat(feature.center).addTo(map);
+    }
+  });
+
+$(window).scroll(function() {
+  if ($(this).scrollTop() > 1) {
+    $("header").addClass("sticky");
+  } else {
+    $("header").removeClass("sticky");
   }
 });
